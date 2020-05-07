f9f6b6eed6f7ea7c8f8f62022c19e3c21934b613
invsc <864645506@qq.com>
2020-05-07 20:11:28 +0800

    feat(/controller/goodscontroller todetail): add new function named toDetail in goodsController

--- src/main/java/com/invsc/miaosha/contrller/GoodsController.java
+++ src/main/java/com/invsc/miaosha/contrller/GoodsController.java
@@ -7,6 +7,7 @@ import com.invsc.miaosha.service.GoodsService;
 import com.invsc.miaosha.service.MiaoshaUserService;
 import com.invsc.miaosha.service.UserService;
 import com.invsc.miaosha.vo.GoodsVo;
+import groovyjarjarcommonscli.MissingOptionException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +15,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -37,14 +39,43 @@ public class GoodsController {
     @RequestMapping("/to_list")
     public String toLogin(Model model, MiaoshaUser user) {
     	model.addAttribute("user", user);
+
     	// 查询商品列表
 	    List<GoodsVo> goodsList = goodsService.listGoodsVo();
 	    model.addAttribute("goodsList", goodsList);
+
         return "goods_list";
     }
-	@RequestMapping("/to_detail")
-	public String toDetail(Model model, MiaoshaUser user) {
+	@RequestMapping("/to_detail/{goodsId}")
+	public String toDetail(Model model, MiaoshaUser user,
+	                       @PathVariable("goodsId") long goodsId) {
+		model.addAttribute("user", user);
 
-		return null;
+		GoodsVo goods = goodsService.getGoodsVoByGoodsId(goodsId);
+		model.addAttribute("goods", goods);
+
+		//
+		long starAt = goods.getStartDate().getTime();
+		long endAt = goods.getEndDate().getTime();
+		long now = System.currentTimeMillis();
+
+		int miaoshaStatus = 0;
+		int remainSeconds = 0;
+
+		if (now < starAt) { // 秒杀没开始
+			miaoshaStatus = 0;
+			remainSeconds = (int) ((starAt - now) / 1000);
+		} else if (now > endAt) { // 秒杀已结束
+			miaoshaStatus = 2;
+			remainSeconds = -1;
+		} else {
+			miaoshaStatus = 1;
+			remainSeconds = 0;
+		}
+
+		model.addAttribute("miaoshaStatus", miaoshaStatus);
+		model.addAttribute("remainSeconds", remainSeconds);
+
+		return "goods_detail";
 	}
 }
